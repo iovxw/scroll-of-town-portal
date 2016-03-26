a1d474003f26de9bc8b293c18ddc2fde0d133a09
iovxw <iovxw@outlook.com>
2016-03-26 21:01:17 +0800

    Fix java.lang.NullPointerException

--- src/scroll_of_town_portal/core.clj
+++ src/scroll_of_town_portal/core.clj
@@ -58,7 +58,7 @@
 
 (defn -onPlayerUse [self event]
   ; 手上拿着卷轴，同时右键了方块或空气
-  (when (and (.isSimilar (.getItem event) @tp-item)
+  (when (and (.isSimilar @tp-item (.getItem event))
              (or (= (.getAction event) Action/RIGHT_CLICK_AIR)
                  (= (.getAction event) Action/RIGHT_CLICK_BLOCK)))
     (let [player (.getPlayer event)
@@ -66,7 +66,7 @@
           inv (.getInventory player)
           bed (.getBedSpawnLocation player)
           in-main-hand? (.equals item (.getItemInMainHand inv))
-          off-hand-has-tp-item? (.isSimilar (.getItemInOffHand inv) @tp-item)]
+          off-hand-has-tp-item? (.isSimilar @tp-item (.getItemInOffHand inv))]
       ; 如果现在使用的是是主手，而且副手也有卷轴，那么不做任何动作
       ; 因为这个Event会同时传给主手和副手，为了防止同时使用
       (when-not (and in-main-hand? off-hand-has-tp-item?)
