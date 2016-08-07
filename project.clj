127437da6493070acf412f47870ba8f9d691ea0e
iovxw <iovxw@outlook.com>
2016-08-07 17:55:55 +0800

    fix bukkit dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :repositories [["spigot-repo" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"]]
+  :profiles {:provided
+             {:dependencies
+              [[org.bukkit/bukkit "1.9-R0.1-SNAPSHOT"]]}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.spigotmc/spigot-api "1.9-R0.1-SNAPSHOT"]
-                 [org.bukkit/bukkit "1.9-R0.1-SNAPSHOT"]]
+                 [org.clojure/data.json "0.2.6"]]
   :aot :all)
